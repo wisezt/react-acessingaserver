79afcff661d6e9b00a68e82a94d02fe6b8c85b70
Ting <wisezt@hotmail.com>
2019-04-21 10:25:54 -0500

    Tutorial 151 Created BurgerIngredient.css

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './Layout.css';
 
 const layout =( props ) =>(
     <>
     <div>Toolbar, SideDrawer, Backdrop </div>
-    <main>
+    <main className= 'Content'>
         {props.children}
         </main>
         </>
