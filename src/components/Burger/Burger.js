4149aef6c5bced8bbc426d394849d96f9a0a1b2b
Ting <wisezt@hotmail.com>
2019-04-21 14:22:15 -0500

    Tutorial 154 Very Important +++ Comments which explains the most complex statement for so far!

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,13 +5,16 @@ import BurgerIngredient from './BurgerIngredient/Burgeringredient.js';
 
 const burger = (props) => {
 
-    const transformedIngredients = Object.keys(props.ingredients)
-            .map((igKey)=>{ return [...Array(props.ingredients[igKey])]
-                .map(( _, i )=>{
-                    return (<BurgerIngredient key={igKey+i} type={igKey} />
+    const transformedIngredients = Object.keys(props.ingredients) // output ["salad", "bacon", "cheese", "meat"], be careful all the elements inside the array are all String!
+            .map((igKey)=>{ return (
+                [...Array(props.ingredients[igKey])] // props.ingredients[igKey] return the number that is the value of the igKey(salad, bacon...) in ingredients!
+                //  the [...Array(props.ingredients[igKey])] return an array will X elements, where the X = number that we talked above
+                .map(( _, i )=> // "_" is the element name which is not important, and i is the index, in here we just use it as the number that how many times we need to loop following statement.
+
+                { return (<BurgerIngredient  key={igKey + i} type={igKey} /> // key is necessary for child list, type will return the specific Burgeringredient
 
                     );
-                }) ;
+                })) ;
         });
 
     return (
