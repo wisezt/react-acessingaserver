5209e0c55a68ed1768ee19dffa72c091b4df272e
Ting <wisezt@hotmail.com>
2019-04-23 10:42:48 -0500

    Tutorial 180 optimized OrderSummary and Modal

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,21 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import classes from './Modal.css';
 import Backdrop from '../Backdrop/Backdrop.js';
 
-const modal = (props) => (
-    <>
-        <Backdrop showed= {props.show} clicked={props.modalClosed} />
+class Modal extends Component {
 
-        <div className={classes.Modal}
-             style={{
-                 transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                 opacity: props.show ? '1' : '0'
-             }}
-        >
-            {props.children}
-        </div>
-    </>
-);
+    shouldComponentUpdate(nextPros, nextState) {
+        return nextPros.show !== this.props.show;
 
-export default modal;
+    }
+
+    componentWillUpdate(){
+        console.log('[Modal] WillUpdate');
+    }
+
+    render() {
+        return(
+            <>
+                <Backdrop showed={this.props.show} clicked={this.props.modalClosed}/>
+
+                <div className={classes.Modal}
+                     style={{
+                         transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                         opacity: this.props.show ? '1' : '0'
+                     }}
+                >
+                    {this.props.children}
+                </div>
+            </>
+        );
+    }
+}
+
+export default Modal;
