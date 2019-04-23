1902ffc21517fd791e97735f68608445e26e9cd9
Ting <wisezt@hotmail.com>
2019-04-23 08:50:56 -0500

    Tutorial 175

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,20 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar.js';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 
-const layout = (props) => (
-    <>
+class Layout extends Component {
+    state ={
+        showSideDrawer: true
+    }
 
-        <Toolbar/>
-        <SideDrawer/>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </>
+    sideDrawerCosedHandler =()=>{
+        this.setState({showSideDrawer: false});
+    }
+
+    render() {
+        return (
+            <>
+
+                <Toolbar/>
+                <SideDrawer
+                    open={this.state.showSideDrawer}
+                    closed = {this.sideDrawerCosedHandler} />
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </>
 
 
-);
+        );
+    }
+}
 
-export default layout;
+export default Layout;
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,19 +2,30 @@ import React from 'react';
 import Logo from '../../Logo/Logo.js';
 import NavigationItems from '../NavigationItems/NavigationItems.js';
 import classes from './SideDrawer.css';
+import Backdrop from '../../UI/Backdrop/Backdrop.js';
 
-const sideDrawer =(props) => {
+const sideDrawer = (props) => {
     // ...
+    let attachedClasses =[classes.SideDrawer, classes.Close];
+
+    if (props.open){
+        attachedClasses =[classes.SideDrawer,  classes.Open];
+    }
+
+
     return (
-        <div className={classes.SideDrawer}>
-            <Logo/>
-            <nav>
+        <>
+
+            <Backdrop showed={props.open}  clicked={props.closed} />
+            <div className={attachedClasses.join(' ')}>
+                <Logo/>
+                <nav>
                     <NavigationItems/>
 
-            </nav>
+                </nav>
 
-        </div>
-
+            </div>
+        </>
 
     );
 };
